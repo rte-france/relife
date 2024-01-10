b3a57c541dc32dd338bdaafc42cecdf9c6ab9099
bdesvignes <benoit.desvignes@artelys.com>
2024-01-10 16:25:25 +0100

    add units tests using specified generated data

--- tests/test_gamma_process.py
+++ tests/test_gamma_process.py
@@ -1,3 +1,24 @@
 import pytest
 
+from relife.datasets import load_insulator_chain_deterioration
+from relife.gamma_process import  GammaProcess
 
+
+@pytest.fixture(scope="module")
+def data():
+    return load_insulator_chain_deterioration()
+
+@pytest.fixture(scope="module")
+def process():
+    return GammaProcess(10, 1)
+
+def test_fit(process, data):
+
+    ids, inspection_times, deterioration_measurements, increments = data
+    process.fit(inspection_times=inspection_times,
+                deterioration_measurements=deterioration_measurements,
+                ids=ids,
+                increments=increments,
+                method='likelihood')
+
+    assert (process.shape_rate.round(2), process.shape_power.round(2), process.rate.round(2)) == (3.24, 1.01, 8.74)
