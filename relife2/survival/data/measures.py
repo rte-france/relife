61a6ff02890315900cca5d48c9fec2d43236e8fc
wgrison <>
2024-04-29 15:53:18 +0200

    refactoring data

--- relife2/survival/data/measures.py
+++ relife2/survival/data/measures.py
@@ -4,16 +4,16 @@ from dataclasses import dataclass
 import numpy as np
 
 
-@dataclass
+@dataclass(frozen=True)
 class Measures:
     values: np.ndarray
     unit_ids: np.ndarray
 
     def __post_init__(self):
-        if len(self.values.shape) != 2:
-            raise ValueError("Invalid Measures values shape")
-        if len(self.unit_ids.shape) != 1:
-            raise ValueError("Invalid Measures unit_ids shape")
+        if self.values.ndim != 2:
+            raise ValueError("Invalid Measures values number of dimensions")
+        if self.unit_ids.ndim != 1:
+            raise ValueError("Invalid Measures unit_ids number of dimensions")
         if len(self.values) != len(self.unit_ids):
             raise ValueError("Incompatible Measures values and unit_ids")
 
@@ -34,35 +34,20 @@ def join_measures(*measures: Measures) -> Measures:
         >>> measures_1 = Measures(values = np.array([[1], [2]]), unit_ids = np.array([3, 10]))
         >>> measures_2 = Measures(values = np.array([[3], [5]]), unit_ids = np.array([10, 2]))
         >>> join_measures(measures_1, measures_2)
-        Measures(values = np.array([[2, 3]]), unit_ids = np.array([10]))
+        Measures(values=array([[2, 3]]), unit_ids=array([10]))
     """
 
-    def join_ids(*ids: np.ndarray):
-        s = set.intersection(*map(lambda x: set(x), ids))
-        return ids[0][masked_ids[0]], [np.in1d(_id, np.array(list(s))) for _id in ids]
+    # def join_ids(*ids: np.ndarray):
+    #     s = set.intersection(*map(lambda x: set(x), ids))
+    #     masked_ids = [np.in1d(_id, np.array(list(s))) for _id in ids]
+    #     return ids[0][masked_ids[0]], masked_ids
 
-    joined_ids, masked_ids = join_ids(*[m.unit_ids for m in measures])
-    return Measures(np.hstack([m.values[masked_ids] for m in measures]), joined_ids)
-
-
-def union_measures(*measures: Measures) -> Measures:
-    """
-    Args:
-        *measures: Measures object.s containing values of shape (n1, p1), (n2, p2), etc.
-
-    Returns:
-        One Measures object where values are concatanation of every unit's values. The result
-        is of shape (n1 + n2 + ..., p1 + p2 + ...).
-
-    Examples:
-        >>> measures_1 = Measures(values = np.array([[1], [2]]), unit_ids = np.array([3, 10]))
-        >>> measures_2 = Measures(values = np.array([[3], [5]]), unit_ids = np.array([10, 2]))
-        >>> join_measures(measures_1, measures_2)
-        Measures(values = np.array([[2, 3]]), unit_ids = np.array([10]))
-    """
+    inter_ids = np.array(
+        list(set.intersection(*map(lambda x: set(x), [m.unit_ids for m in measures])))
+    )
     return Measures(
-        np.hstack([m.values for m in measures]),
-        np.hstack([m.unit_ids for m in measures]),
+        np.hstack([m.values[np.isin(m.unit_ids, inter_ids)] for m in measures]),
+        inter_ids,
     )
 
 
@@ -100,125 +85,112 @@ class MeasuresParser(ABC):
         pass
 
     @staticmethod
-    def _sanity_checks(
-            result: tuple[
-                Measures, Measures, Measures, Measures, Measures, Measures, Measures
-            ]
+    def _check_interval_censorships(interval_censorships: Measures) -> None:
+        if len(interval_censorships) != 0:
+            if (
+                    interval_censorships.values[:, 0] >= interval_censorships.values[:, 1]
+            ).any():
+                raise ValueError(
+                    "Interval censorships lower bounds can't be higher or equal to its upper bounds"
+                )
+
+    @staticmethod
+    def _check_interval_truncations(interval_truncations: Measures) -> None:
+        if len(interval_truncations) != 0:
+            if (
+                    interval_truncations.values[:, 0] >= interval_truncations.values[:, 1]
+            ).any():
+                raise ValueError(
+                    "Interval truncations lower bounds can't be higher or equal to its upper bounds"
+                )
+
+    @staticmethod
+    def _compatible_with_left_truncations(
+            lifetimes: Measures, left_truncations: Measures
     ) -> None:
-        (
-            complete_lifetimes,
-            left_censorships,
-            right_censorships,
-            interval_censorships,
-            left_truncations,
-            right_truncations,
-            interval_truncations,
-        ) = result
+        if len(lifetimes) != 0 and len(left_truncations) != 0:
+            joined_measures = join_measures(lifetimes, left_truncations)
+            if len(joined_measures) != 0:
+                if (
+                        np.min(
+                            joined_measures.values[:, : lifetimes.values.shape[-1]],
+                            axis=1,
+                            keepdims=True,
+                        )
+                        < joined_measures.values[:, lifetimes.values.shape[-1]:]
+                ).any():
+                    raise ValueError(
+                        f"""
+                        Some lifetimes are under left truncation bounds :
+                        {lifetimes} and {left_truncations}
+                        """
+                    )
 
-        if (
-                interval_censorships.values[:, 0] >= interval_censorships.values[:, 1]
-        ).any():
-            raise ValueError("Invalid interval censorship values")
+    @staticmethod
+    def _compatible_with_right_truncations(
+            lifetimes: Measures, right_truncations: Measures
+    ) -> None:
+        if len(lifetimes) != 0 and len(right_truncations) != 0:
+            joined_measures = join_measures(lifetimes, right_truncations)
+            if len(joined_measures) != 0:
+                if (
+                        np.max(
+                            joined_measures.values[:, : lifetimes.values.shape[-1]],
+                            axis=1,
+                            keepdims=True,
+                        )
+                        > joined_measures.values[:, 1]
+                ).any():
+                    raise ValueError(
+                        f"""
+                        Some lifetimes are above right truncation bounds :
+                        {lifetimes} and {right_truncations}
+                        """
+                    )
 
-        if (
-                interval_truncations.values[:, 0] >= interval_truncations.values[:, 1]
-        ).any():
-            raise ValueError("Invalid interval truncation values")
-
-        joined_measures = join_measures(right_censorships, left_truncations)
-        if (joined_measures.values[:, 0] <= joined_measures.values[:, 1]).any():
-            raise ValueError(
-                f"""
-                right censored lifetime values can't be lower or equal to left
-                truncation values:
-                incompatible {right_censorships} and {left_truncations}
-                """
-            )
-
-        joined_measures = join_measures(right_censorships, interval_truncations)
-        if (joined_measures.values[:, 0] >= joined_measures.values[:, 2]).any():
-            raise ValueError(
-                f"""
-                  right censored lifetime values can't be higer or equal to interval
-                  of truncation: incompatible {right_censorships} and {interval_truncations}
-                  """
-            )
-
-        joined_measures = join_measures(interval_censorships, interval_truncations)
-        if (joined_measures.values[:, 0] < joined_measures.values[:, 2]).any():
-            raise ValueError(
-                f"""
-                  interval censorship can't be outside of truncation interval:
-                  incompatible {interval_censorships} and {interval_truncations}
-                  """
-            )
-        elif (joined_measures.values[:, 1] > joined_measures.values[:, 3]).any():
-            raise ValueError(
-                f"""
-                  interval censorship can't be outside of truncation interval:
-                  incompatible {interval_censorships} and {interval_truncations}
-                  """
-            )
-
-        joined_measures = join_measures(right_censorships, right_truncations)
-        if (joined_measures.values[:, 0] >= joined_measures.values[:, 1]).any():
-            raise ValueError(
-                f"""
-                  right censored lifetime values can't be higher than right truncations:
-                  incompatible {right_censorships} and {right_truncations}
-                  """
-            )
-
-        joined_measures = join_measures(left_censorships, right_truncations)
-        if (joined_measures.values[:, 0] >= joined_measures.values[:, 1]).any():
-            raise ValueError(
-                f"""
-                  left censored lifetime values can't be higher than right truncations:
-                  incompatible {left_censorships} and {right_truncations}
-                  """
-            )
-
-        joined_measures = join_measures(interval_censorships, right_truncations)
-        if (joined_measures.values[:, 1] >= joined_measures.values[:, 2]).any():
-            raise ValueError(
-                f"""
-                  interval censored lifetime values can't be higher than right truncations
-                  : incompatible {interval_censorships} and {right_truncations}
-                  """
-            )
-
-        joined_measures = join_measures(complete_lifetimes, right_truncations)
-        if (joined_measures.values[:, 0] >= joined_measures.values[:, 1]).any():
-            raise ValueError(
-                f"""
-                  complete lifetime values can't be higher than right truncations:
-                  incompatible {complete_lifetimes} and {right_truncations}
-                  """
-            )
-
-        joined_measures = join_measures(complete_lifetimes, left_truncations)
-        if (joined_measures.values[:, 0] <= joined_measures.values[:, 1]).any():
-            raise ValueError(
-                f"""
-                  complete lifetime values can't be lower than left truncations:
-                  incompatible {complete_lifetimes} and {left_truncations}
-                  """
-            )
-
-        joined_measures = join_measures(complete_lifetimes, interval_truncations)
-        if (joined_measures.values[:, 0] >= joined_measures.values[:, 2]).any():
-            raise ValueError(
-                f"""
-                  complete lifetime values can't be outside of truncation interval:
-                  incompatible {complete_lifetimes} and {interval_truncations}
-                  """
-            )
-        elif (joined_measures.values[:, 0] <= joined_measures.values[:, 1]).any():
-            raise ValueError(
-                f"""
-                  complete lifetime values can't be outside of truncation interval:
-                  incompatible {complete_lifetimes} and {interval_truncations}"""
-            )
+    @staticmethod
+    def _compatible_with_interval_truncations(
+            lifetimes: Measures, interval_truncations: Measures
+    ) -> None:
+        if len(lifetimes) != 0 and len(interval_truncations) != 0:
+            joined_measures = join_measures(lifetimes, interval_truncations)
+            if len(joined_measures) != 0:
+                if (
+                        np.max(
+                            joined_measures.values[:, : lifetimes.values.shape[-1]],
+                            axis=1,
+                            keepdims=True,
+                        )
+                        > np.max(
+                    joined_measures.values[:, lifetimes.values.shape[-1]:],
+                    axis=1,
+                    keepdims=True,
+                )
+                ).any():
+                    raise ValueError(
+                        f"""
+                        Some lifetimes above interval truncation bounds :
+                        {lifetimes} and {interval_truncations}
+                        """
+                    )
+                elif (
+                        np.min(
+                            joined_measures.values[:, : lifetimes.values.shape[-1]],
+                            axis=1,
+                            keepdims=True,
+                        )
+                        < np.min(
+                    joined_measures.values[:, lifetimes.values.shape[-1]:],
+                    axis=1,
+                    keepdims=True,
+                )
+                ).any():
+                    raise ValueError(
+                        f"""
+                        Some lifetimes below interval truncation bounds :
+                        {lifetimes} and {interval_truncations}
+                        """
+                    )
 
     def __call__(
             self,
@@ -233,9 +205,14 @@ class MeasuresParser(ABC):
             self.get_interval_truncations(),
         )
         try:
-            MeasuresParser._sanity_checks(result)
+            MeasuresParser._check_interval_censorships(result[3])
+            MeasuresParser._check_interval_truncations(result[6])
+            for lifetimes in result[:4]:
+                MeasuresParser._compatible_with_left_truncations(lifetimes, result[4])
+                MeasuresParser._compatible_with_right_truncations(lifetimes, result[5])
+                MeasuresParser._compatible_with_right_truncations(lifetimes, result[6])
         except Exception as error:
-            raise ValueError("Invalid input measures") from error
+            raise ValueError("Incorrect input measures") from error
 
         return result
 
@@ -252,14 +229,14 @@ class MeasuresParserFrom1D(MeasuresParser):
 
         (n,) = time.shape
 
-        if lc_indicators:
+        if lc_indicators is not None:
             lc_indicators = lc_indicators.astype(np.bool_, copy=False)
             if lc_indicators.shape != (n,):
                 raise ValueError(f"invalid lc_indicators shape, expected ({n},)")
         else:
             lc_indicators = np.zeros_like(time, dtype=np.bool_)
 
-        if rc_indicators:
+        if rc_indicators is not None:
             rc_indicators = rc_indicators.astype(np.bool_, copy=False)
             if rc_indicators.shape != (n,):
                 raise ValueError(f"invalid rc_indicators shape, expected ({n},)")
@@ -273,13 +250,13 @@ class MeasuresParserFrom1D(MeasuresParser):
                 """
             )
 
-        if entry:
+        if entry is not None:
             if entry.shape != (n,):
                 raise ValueError(f"invalid entry shape, expected ({n},)")
         else:
             entry = np.empty((0, 1), dtype=float)
 
-        if departure:
+        if departure is not None:
             if departure.shape != (n,):
                 raise ValueError(f"invalid departure shape, expected ({n},)")
         else:
@@ -311,12 +288,12 @@ class MeasuresParserFrom1D(MeasuresParser):
 
     def get_left_truncations(self) -> Measures:
         index = np.where(self.entry != 0)[0]
-        values = self.entry[index]
+        values = self.entry[index].reshape(-1, 1)
         return Measures(values, index)
 
     def get_right_truncations(self) -> Measures:
         index = np.where(self.departure != 0)[0]
-        values = self.departure[index]
+        values = self.departure[index].reshape(-1, 1)
         return Measures(values, index)
 
     def get_interval_truncations(self) -> Measures:
@@ -379,12 +356,12 @@ class MeasuresParserFrom2D(MeasuresParser):
 
     def get_left_truncations(self) -> Measures:
         index = np.where(self.entry != 0)[0]
-        values = self.entry[index]
+        values = self.entry[index].reshape(-1, 1)
         return Measures(values, index)
 
     def get_right_truncations(self) -> Measures:
         index = np.where(self.departure != 0)[0]
-        values = self.departure[index]
+        values = self.departure[index].reshape(-1, 1)
         return Measures(values, index)
 
     def get_interval_truncations(self) -> Measures:
