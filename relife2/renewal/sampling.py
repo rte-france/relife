91eb9aa5b8236865651b4a53023923e13a70e98a
wgrison <>
2024-10-18 11:36:02 +0200

    add order attribute to CountData, seperate Simulator in simulation module

--- relife2/renewal/sampling.py
+++ relife2/renewal/sampling.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass, field, fields
-from functools import singledispatchmethod
 from typing import Optional, TypeVarTuple, Iterator
 
 import numpy as np
@@ -7,13 +6,6 @@ from numpy.typing import NDArray
 
 from relife2.model import LifetimeModel
 from relife2.renewal.discountings import Discounting
-from relife2.renewal.policy import (
-    Policy,
-    OneCycleRunToFailure,
-    OneCycleAgeReplacementPolicy,
-    RunToFailure,
-)
-from relife2.renewal.process import RenewalProcess, RenewalRewardProcess
 from relife2.renewal.rewards import Reward
 
 ModelArgs = TypeVarTuple("ModelArgs")
@@ -70,18 +62,18 @@ def lifetimes_generator(
 ]:
 
     event_times = np.zeros((nb_assets, nb_samples))
-    events = np.zeros((nb_assets, nb_samples))
+    order = np.zeros((nb_assets, nb_samples))
     still_valid = event_times < end_time
 
     def sample_routine(target_model, args):
-        nonlocal event_times, events, still_valid  # modify these variables
+        nonlocal event_times, order, still_valid  # modify these variables
         lifetimes = model_rvs(target_model, size, args=args).reshape(
             (nb_assets, nb_samples)
         )
         event_times += lifetimes
-        events += 1
+        order += 1
         still_valid = event_times < end_time
-        return lifetimes, event_times, events, still_valid
+        return lifetimes, event_times, order, still_valid
 
     if delayed_model:
         size = rvs_size(nb_samples, nb_assets, delayed_model_args)
@@ -139,11 +131,11 @@ def lifetimes_rewards_generator(
 
     def sample_routine(target_reward, args):
         nonlocal total_rewards  # modify these variables
-        lifetimes, event_times, events, still_valid = next(lifetimes_gen)
+        lifetimes, event_times, order, still_valid = next(lifetimes_gen)
         rewards = target_reward(lifetimes, *args)
         discountings = discounting.factor(event_times, *discount_args)
         total_rewards += rewards * discountings
-        return lifetimes, event_times, total_rewards, events, still_valid
+        return lifetimes, event_times, total_rewards, order, still_valid
 
     if delayed_reward is None:
         delayed_reward = reward
@@ -165,15 +157,19 @@ def lifetimes_rewards_generator(
 
 @dataclass
 class CountData:
-    samples: NDArray[np.int64] = field(repr=False)
-    assets: NDArray[np.int64] = field(repr=False)
-    events: NDArray[np.int64] = field(repr=False)
+    samples: NDArray[np.int64] = field(repr=False)  # samples index
+    assets: NDArray[np.int64] = field(repr=False)  # assets index
+    order: NDArray[np.int64] = field(repr=False)  # order index
     event_times: NDArray[np.float64] = field(repr=False)
 
     nb_samples: int = field(init=False)
     nb_assets: int = field(init=False)
-    samples_index: NDArray[np.int64] = field(init=False, repr=False)
-    assets_index: NDArray[np.int64] = field(init=False, repr=False)
+    samples_index: NDArray[np.int64] = field(
+        init=False, repr=False
+    )  # unique samples index
+    assets_index: NDArray[np.int64] = field(
+        init=False, repr=False
+    )  # unique assets index
 
     def __post_init__(self):
         fields_values = [
@@ -188,6 +184,9 @@ class CountData:
         self.assets_index = np.unique(self.assets)
         self.nb_samples = len(self.samples_index)
         self.nb_assets = len(self.assets_index)
+
+    def __len__(self) -> int:
+        return self.nb_samples * self.nb_assets
 
     def number_of_events(
         self, sample: int
@@ -211,7 +210,7 @@ class CountDataIterable:
         self.data = data
 
         sorted_index = np.lexsort(
-            (self.data.events, self.data.assets, self.data.samples)
+            (self.data.order, self.data.assets, self.data.samples)
         )
         self.sorted_fields = {
             _field.name: getattr(self.data, _field.name).copy()[sorted_index]
@@ -259,253 +258,3 @@ class RenewalRewardData(RenewalData):
         times = np.insert(self.event_times[s], 0, 0)
         z = np.insert(self.total_rewards[s].cumsum(), 0, 0) / self.nb_samples
         return times, z
-
-
-class Sampler:
-    """
-    benefits of decoupling sample from other class:
-    1. avoid polluting RenewalProcess with sample making inheritance
-    between RenewalProcess and RenewalRewardProcess wrong (LSP) : signature overridden because of reward_args, etc.
-    2. avoid having sample method that returns different dataclass and possibly non "to_lifetime_data" compatible
-    => all sample returns CountData type that can be converted to lifetime data (easier to understand)
-    3. meaningly speaking, one may not expect RenewalProcess or Policy object having a "sample" in its interface
-    4. (3 bis) if RenewalProcess and Policy have "sample", why not LifetimeModel ? => Sampler accept LifetimeModel too
-    """
-
-    def __init__(
-        self,
-        end_time: float,
-        nb_sample: int,
-        *,
-        random_state: Optional[int] = None,
-    ):
-        self.end_time = end_time
-        self.nb_sample = nb_sample
-        self.random_state = random_state
-        self.sample_result, self.events, self.args, self.a0 = None, None, None, None
-
-    def run(
-        self, target_obj: LifetimeModel | RenewalProcess | Policy, **kwargs
-    ) -> CountData:
-        """
-        Args:
-            target_obj (): obj used to sample
-            **kwargs (): arguments needed by policy or renewal process
-        Returns:
-        """
-        try:
-            self.sample_result, self.events, self.args, self.a0 = self.sample(
-                target_obj, **kwargs
-            )
-        except NotImplementedError:
-            raise NotImplementedError(
-                f"Cannot run sampler on {type(target_obj)} object"
-            )
-        return self.sample_result
-
-    @singledispatchmethod
-    def sample(
-        self,
-        target_obj,
-        **kwargs,
-    ) -> tuple[
-        CountData,
-        Optional[NDArray[np.float64]],
-        Optional[NDArray[np.float64]],
-        Optional[NDArray[np.float64]],
-    ]:
-        """
-        Args:
-            target_obj (): obj used to sample
-            **kwargs (): arguments needed by policy or renewal process
-        Returns:
-        """
-        raise NotImplementedError
-
-    @sample.register
-    def _(self, target_obj: LifetimeModel, **kwargs):
-        pass
-
-    @sample.register
-    def _(self, target_obj: RenewalProcess, **kwargs):
-        pass
-
-    @sample.register
-    def _(self, target_obj: RenewalRewardProcess, **kwargs):
-        pass
-
-    @sample.register
-    def _(self, target_obj: OneCycleRunToFailure, **kwargs):
-        pass
-
-    @sample.register
-    def _(self, target_obj: OneCycleAgeReplacementPolicy, **kwargs):
-        pass
-
-    @sample.register
-    def _(self, target_obj: RunToFailure, **kwargs):
-        pass
-
-    def to_lifetime_data(self):
-        pass
-
-        # if tf is None or tf > self.T:
-        #     tf = self.T
-        # if t0 >= tf:
-        #     raise ValueError("`t0` must be strictly lesser than `tf`")
-        #
-        # # Filtering sample and sorting by times
-        # s = self.samples == sample if sample is not None else Ellipsis
-        # order = np.argsort(self.times[s])
-        # indices = self.indices[s][order]
-        # samples = self.samples[s][order]
-        # uindices = np.ravel_multi_index(
-        #     (indices, samples), (self.n_indices, self.n_samples)
-        # )
-        # times = self.times[s][order]
-        # durations = self.durations[s][order] + self.a0[s][order]
-        # events = self.events[s][order]
-        #
-        # # Indices of interest
-        # ind0 = (times > t0) & (
-        #     times <= tf
-        # )  # Indices of replacement occuring inside the obervation window
-        # ind1 = (
-        #     times > tf
-        # )  # Indices of replacement occuring after the observation window which include right censoring
-        #
-        # # Replacements occuring inside the observation window
-        # time0 = durations[ind0]
-        # event0 = events[ind0]
-        # entry0 = np.zeros(time0.size)
-        # _, LT = np.unique(
-        #     uindices[ind0], return_index=True
-        # )  # get the indices of the first replacements ocurring in the observation window
-        # b0 = (
-        #     times[ind0][LT] - durations[ind0][LT]
-        # )  # time at birth for the firt replacements
-        # entry0[LT] = np.where(b0 >= t0, 0, t0 - b0)
-        # args0 = args_take(indices[ind0], *self.args)
-        #
-        # # Right censoring
-        # _, RC = np.unique(uindices[ind1], return_index=True)
-        # bf = (
-        #     times[ind1][RC] - durations[ind1][RC]
-        # )  # time at birth for the right censored
-        # b1 = bf[
-        #     bf < tf
-        # ]  # ensure that time of birth for the right censored is not equal to tf.
-        # time1 = tf - b1
-        # event1 = np.zeros(b1.size)
-        # entry1 = np.where(b1 >= t0, 0, t0 - b1)
-        # args1 = args_take(indices[ind1][RC][bf < tf], *self.args)
-        #
-        # # Concatenate
-        # time = np.concatenate((time0, time1))
-        # event = np.concatenate((event0, event1))
-        # entry = np.concatenate((entry0, entry1))
-        # args = tuple(
-        #     np.concatenate((arg0, arg1), axis=0) for arg0, arg1 in zip(args0, args1)
-        # )
-        # return LifetimeData(time, event, entry, args)
-
-
-#     lifetimes = np.array([], dtype=np.float64)
-#     failure_times = np.array([], dtype=np.float64)
-#     samples = np.array([], dtype=np.int64)
-#     assets = np.array([], dtype=np.int64)
-#
-#     for step, (_lifetimes, _failure_times, still_valid) in enumerate(
-#         lifetimes_generator(
-#             model,
-#             nb_samples,
-#             nb_assets,
-#             end_time,
-#             model_args=model_args,
-#             delayed_model=delayed_model,
-#             delayed_model_args=delayed_model_args,
-#         )
-#     ):
-#         lifetimes = np.concatenate((lifetimes, _lifetimes[still_valid].reshape(-1)))
-#         failure_times = np.concatenate(
-#             (failure_times, _failure_times[still_valid].reshape(-1))
-#         )
-#         _assets, _samples = np.where(still_valid)
-#         samples = np.concatenate((samples, _samples))
-#         assets = np.concatenate((assets, _assets))
-#         if nb_step:
-#             if step + 1 == nb_step:
-#                 break
-#
-#     sorted_index = np.lexsort((assets, samples))
-#     lifetimes = lifetimes[sorted_index]
-#     failure_times = failure_times[sorted_index]
-#     samples = samples[sorted_index]
-#     assets = assets[sorted_index]
-#
-
-
-#
-#     if nb_step is None and end_time is None:
-#         raise ValueError("nb_step or end_time must be given")
-#     elif nb_step is not None and end_time:
-#         raise ValueError("can't have nb_step and end_time given together")
-#     if nb_step:
-#         end_time = np.inf
-#
-#     failure_times = np.array([], dtype=np.float64)
-#     lifetimes = np.array([], dtype=np.float64)
-#     total_rewards = np.array([], dtype=np.float64)
-#     samples = np.array([], dtype=np.int64)
-#     assets = np.array([], dtype=np.int64)
-#
-#     for step, (
-#         _lifetimes,
-#         _failure_times,
-#         _total_rewards,
-#         still_valid,
-#     ) in enumerate(
-#         lifetimes_rewards_generator(
-#             model,
-#             reward,
-#             discounting,
-#             nb_samples,
-#             nb_assets,
-#             end_time,
-#             model_args=model_args,
-#             reward_args=reward_args,
-#             discount_args=discount_args,
-#             delayed_model=delayed_model,
-#             delayed_model_args=delayed_model_args,
-#             delayed_reward=delayed_reward,
-#             delayed_reward_args=delayed_reward_args,
-#         )
-#     ):
-#         lifetimes = np.concatenate((lifetimes, _lifetimes[still_valid].reshape(-1)))
-#         failure_times = np.concatenate(
-#             (failure_times, _failure_times[still_valid].reshape(-1))
-#         )
-#         total_rewards = np.concatenate(
-#             (total_rewards, _total_rewards[still_valid].reshape(-1))
-#         )
-#         _assets, _samples = np.where(still_valid)
-#         samples = np.concatenate((samples, _samples))
-#         assets = np.concatenate((assets, _assets))
-#         if nb_step:
-#             if step + 1 == nb_step:
-#                 break
-#
-#     sorted_index = np.lexsort((assets, samples))
-#     lifetimes = lifetimes[sorted_index]
-#     failure_times = failure_times[sorted_index]
-#     total_rewards = total_rewards[sorted_index]
-#     samples = samples[sorted_index]
-#     assets = assets[sorted_index]
-#
-#     super().__init__(
-#         samples,
-#         assets,
-#         lifetimes,
-#         failure_times,
-#         total_rewards,
-#     )
