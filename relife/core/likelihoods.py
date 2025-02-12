rm hessian scheme from concrete class and fix control of params set in ParametricLifetimeModel

relife/core/likelihoods.py
@@ -9,11 +9,75 @@ from scipy.optimize import approx_fprime
 from typing_extensions import override
 
 from relife.data import LifetimeData
-from ..models import Gamma
-from ..models.regression import Regression
 
 if TYPE_CHECKING:  # avoid circular imports due to typing
     from .model import ParametricLifetimeModel, ParametricModel
+
+
+def hessian_cs(
+    likelihood: Likelihood,
+    eps: float = 1e-6,
+) -> Union[NDArray[np.float64], None]:
+    """
+
+    Args:
+        likelihood ():
+        eps ():
+
+    Returns:
+
+    """
+    if likelihood.hasjac:
+        size = likelihood.params.size
+        hess = np.empty((size, size))
+        u = eps * 1j * np.eye(size)
+        params = likelihood.params.copy()
+        for i in range(size):
+            for j in range(i, size):
+                hess[i, j] = (
+                    np.imag(likelihood.jac_negative_log(params + u[i])[j]) / eps
+                )
+                if i != j:
+                    hess[j, i] = hess[i, j]
+        return hess
+    return None
+
+
+def hessian_2point(
+    likelihood: Likelihood,
+    eps: float = 1e-6,
+) -> Union[NDArray[np.float64], None]:
+    """
+
+    Args:
+        likelihood ():
+        eps ():
+
+    Returns:
+
+    """
+    if likelihood.hasjac:
+        size = likelihood.params.size
+        params = likelihood.params.copy()
+        hess = np.empty((size, size))
+        for i in range(size):
+            hess[i] = approx_fprime(
+                params,
+                lambda x: likelihood.jac_negative_log(x)[i],
+                eps,
+            )
+        return hess
+    return None
+
+
+def _hessian_scheme(obj: ParametricLifetimeModel):
+    from relife.models import Gamma
+
+    if hasattr(obj, "baseline"):
+        return _hessian_scheme(obj.baseline)
+    if isinstance(obj, Gamma):
+        return hessian_2point
+    return hessian_cs
 
 
 class Likelihood(Protocol):
@@ -26,6 +90,9 @@ class Likelihood(Protocol):
     @property
     def hasjac(self) -> bool:
         return False
+
+    def hessian(self, eps: float = 1e-6) -> NDArray[np.float64]:
+        return _hessian_scheme(self.model)(self, eps=eps)
 
     @abstractmethod
     def negative_log(self, params: NDArray[np.float64]) -> float:
@@ -231,62 +298,6 @@ class LikelihoodFromLifetimes(Likelihood):
         )
 
 
-def hessian_cs(
-    likelihood: Likelihood,
-    eps: float = 1e-6,
-) -> Union[NDArray[np.float64], None]:
-    """
-
-    Args:
-        likelihood ():
-        eps ():
-
-    Returns:
-
-    """
-    if likelihood.hasjac:
-        size = likelihood.params.size
-        hess = np.empty((size, size))
-        u = eps * 1j * np.eye(size)
-        params = likelihood.params.copy()
-        for i in range(size):
-            for j in range(i, size):
-                hess[i, j] = (
-                    np.imag(likelihood.jac_negative_log(params + u[i])[j]) / eps
-                )
-                if i != j:
-                    hess[j, i] = hess[i, j]
-        return hess
-    return None
-
-
-def hessian_2point(
-    likelihood: Likelihood,
-    eps: float = 1e-6,
-) -> Union[NDArray[np.float64], None]:
-    """
-
-    Args:
-        likelihood ():
-        eps ():
-
-    Returns:
-
-    """
-    if likelihood.hasjac:
-        size = likelihood.params.size
-        params = likelihood.params.copy()
-        hess = np.empty((size, size))
-        for i in range(size):
-            hess[i] = approx_fprime(
-                params,
-                lambda params: likelihood.jac_negative_log(params)[i],
-                eps,
-            )
-        return hess
-    return None
-
-
 def hessian_from_likelihood(method: str):
     match method:
         case "2-point":
@@ -295,11 +306,3 @@ def hessian_from_likelihood(method: str):
             return hessian_cs
         case _:
             return hessian_2point
-
-
-def hessian_scheme(obj: ParametricLifetimeModel):
-    if hasattr(obj, "baseline"):
-        return hessian_scheme(obj.baseline)
-    if isinstance(obj, Gamma):
-        return hessian_2point
-    return hessian_cs

relife/core/model.py
@@ -208,10 +208,6 @@ class ParametricModel:
         """
         return len(self._params)
 
-    @property
-    def _all_params_set(self):
-        return np.isnan(self.params).any() and not np.isnan(self.params).all()
-
     def compose_with(self, **kwcomponents: Self):
         """Compose with new ``ParametricModel`` instance(s).
 
@@ -856,26 +852,6 @@ class FittingResults:
 
 
 class ParametricLifetimeModel(LifetimeModel[*VariadicArgs], ParametricModel, ABC):
-    """
-    Base class to create a parametric lifetime core
-
-    A parametric lifetime core is an ``LifetimeModel`` having parameters that can
-    be estimated, i.e. it has a `fit` method.
-    """
-
-    # def __init_subclass__(cls, **kwargs):
-    #     """
-    #     TODO : something to parse *args names and to fill args_names and nb_args
-    #     see Descriptors
-    #     """
-    #
-    # @property
-    # def args_names(self):
-    #     return self._args.names
-    #
-    # @property
-    # def nb_args(self):
-    #     return len(self._args)
 
     fitting_results: FittingResults | None
 
@@ -907,10 +883,6 @@ class ParametricLifetimeModel(LifetimeModel[*VariadicArgs], ParametricModel, ABC
         args : tuple of numpy arrays
             lala.
         """
-
-    @property
-    def _default_hess_scheme(self) -> str:
-        return "cs"
 
     def fit(
         self,
@@ -992,9 +964,7 @@ class ParametricLifetimeModel(LifetimeModel[*VariadicArgs], ParametricModel, ABC
             **minimize_kwargs,
         )
         optimized_model.params = optimizer.x
-        var = np.linalg.inv(
-            hessian_from_likelihood(self._default_hess_scheme)(likelihood)
-        )
+        var = np.linalg.inv(likelihood.hessian())
 
         optimized_model.fitting_results = FittingResults(
             len(lifetime_data), optimizer, var
@@ -1011,12 +981,12 @@ class ParametricLifetimeModel(LifetimeModel[*VariadicArgs], ParametricModel, ABC
 
         if (
             not item.startswith("_")
-            and not not item.startswith("__")
+            and not item.startswith("__")
             and hasattr(LifetimeModel, item)
         ):
-            if not self._all_params_set:
+            if None in self.params:
                 raise ValueError(
-                    f"Can't call {item} if one core params is not set. Instanciate fully parametrized core or fit it"
+                    f"Can't call {item} if one param is None. Got {self.params} as params"
                 )
         return super().__getattribute__(item)
 