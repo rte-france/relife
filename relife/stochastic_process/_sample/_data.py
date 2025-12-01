00bf8e3d8a55a08ccbd1c85871d000f3e2511cd5
Nathan Herzhaft <nathanherzhaft@yahoo.fr>
2025-12-01 14:00:02 +0100

    Docstring

--- relife/stochastic_process/_sample/_data.py
+++ relife/stochastic_process/_sample/_data.py
@@ -22,29 +22,40 @@ class StochasticDataSample:
     _col_timeline: NDArray[np.int64] = None
 
     def __post_init__(self):
+
+        # assets x samples are placed on axis 0
         index_in_rows = self._struct_array["asset_id"] * self.nb_samples + self._struct_array["sample_id"]
         unique_index, self._row_index = np.unique(index_in_rows, return_inverse=True)
 
+        # unique values of timeline on axis 1
         self.timeline, self._col_timeline = np.unique(self._struct_array["timeline"], return_inverse=True)
 
-        data_matrix = np.zeros(
+        # 2D matrix of booleans to indicate observed events
+        self.events = np.zeros(
             (self.nb_assets * self.nb_samples, self.timeline.size), dtype=bool
         )
-
-        self.events = data_matrix.copy()
         self.events[self._row_index, self._col_timeline] = self._struct_array["event"]
 
-        self.preventive_renewals = data_matrix.copy()
+        # 2D matrix of booleans to indicate preventive renewals
+        self.preventive_renewals = np.zeros(
+            (self.nb_assets * self.nb_samples, self.timeline.size), dtype=bool
+        )
         self.preventive_renewals[self._row_index, self._col_timeline] = ~self._struct_array["event"]
         self.preventive_renewals[:, -1] = False
 
     def _select_with_mask(self, mask)-> StochasticDataSample:
+        """
+        Take sub-matrix on specific assets and samples using a mask
+        """
         return replace(self, events=self.events[mask], preventive_renewals=self.preventive_renewals[mask])
 
     def select(
         self, sample_id: Optional[Union[int|List[int]]] = None, asset_id: Optional[Union[int|List[int]]] = None
     ) -> StochasticDataSample:
-    
+        """
+        Focus on specific assets and samples. Return a truncated StochasticDataSample.
+        """
+
         if asset_id is None:
             asset_id = np.arange(self.nb_assets)
         if sample_id is None:
@@ -61,6 +72,9 @@ class StochasticDataSample:
         return replace(self._select_with_mask(mask),nb_assets=nb_assets,nb_samples=nb_samples)
     
     def _select_from_struct(self, sample_id: Optional[Union[int|List[int]]] = None, asset_id: Optional[Union[int|List[int]]] = None):
+        """
+        Select the samples and assets in the struct array. Used for dev and tests only.
+        """
         mask: NDArray[np.bool_] = np.ones_like(self._struct_array, dtype=np.bool_)
         if sample_id is not None:
             mask = mask & np.isin(self._struct_array["sample_id"], sample_id)
