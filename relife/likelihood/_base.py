FIX - hessian conditions for covariance matrix derivation

relife/likelihood/_base.py
@@ -241,6 +241,7 @@ class Likelihood(ABC):
             "bounds": optimizer_options.pop("bounds", None),
             "x0": optimizer_options.pop("x0", self.params),
         }
+        print(f"x0: {minimize_kwargs['x0']}")
         minimize_kwargs.update(optimizer_options)
         optimizer = minimize(
             self.negative_log,
@@ -254,13 +255,14 @@ class Likelihood(ABC):
             **minimize_kwargs,
         )
         optimal_params = np.copy(optimizer.x)
+        print(f"xend: {optimal_params}")
         neg_log_likelihood = np.copy(
             optimizer.fun
         )  # neg_log_likelihood value at optimal
         if (
                 (minimize_kwargs["method"] in SCIPY_MINIMIZE_ORDER_2_ALGO)
                 and ("hess" in minimize_kwargs)
-                and (minimize_kwargs["hess"] is not None)
+                and callable(minimize_kwargs["hess"])
         ):
             hessian = minimize_kwargs["hess"](optimal_params)
         else:

relife/likelihood/_lifetime_likelihood.py
@@ -484,12 +484,12 @@ class PartialLifetimeLikelihood(Likelihood):
         self.params = params  # changes model params
 
         if self.method == "cox":
-            return -(
+            neg_L = -(
                     np.log(self.model.covar_effect.g(self._ordered_event_covar)).sum()
                     - np.log(self._psi()).sum()
             )
         elif self.method == "breslow":
-            return -(
+            neg_L = -(
                     np.log(self.model.covar_effect.g(self._s_j)).sum()
                     - (self._event_count[:, None] * np.log(self._psi())).sum()
             )
@@ -498,13 +498,14 @@ class PartialLifetimeLikelihood(Likelihood):
             # .sum() --> sum on j
             # using where in np.log allows to avoid 0. masked elements
             m = self._psi_efron()
-            neg_L_efron = -(
+            neg_L = -(
                     np.log(self.model.covar_effect.g(self._s_j)).sum()
                     - np.log(m, out=np.zeros_like(m), where=(m != 0))
                     .sum(axis=1, keepdims=True)
                     .sum()
             )
-            return neg_L_efron
+        print(f"neg_L: {neg_L}")
+        return neg_L
 
     def jac_negative_log(
             self,