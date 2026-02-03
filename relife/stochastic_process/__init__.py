fed83a9caccde902ea89d3f7167d34681a06504a
Nathan Herzhaft <nathanherzhaft@yahoo.fr>
2026-02-03 15:37:08 +0100

    TYPO in __init__ imports

--- relife/stochastic_process/__init__.py
+++ relife/stochastic_process/__init__.py
@@ -2,6 +2,6 @@ from .non_homogeneous_poisson_process import (
     NonHomogeneousPoissonProcess,
 )
 from .renewal_process import RenewalProcess, RenewalRewardProcess
-from .kijima_process import KijimaIPRocess, KijimaIIProcess
+from .kijima_process import KijimaIProcess, KijimaIIProcess
 
 __all__ = ["RenewalProcess", "RenewalRewardProcess", "NonHomogeneousPoissonProcess", "KijimaIProcess", "KijimaIIProcess"]
