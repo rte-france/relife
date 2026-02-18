805370b372043e3f315ef854088b02dee9f55272
abossavy <arthur.bossavy@artelys.com>
2026-02-18 17:57:20 +0100

    DELETE - likelihood/

--- relife/likelihood/__init__.py
+++
@@ -1,4 +0,0 @@
-from ._lifetime_likelihood import IntervalLifetimeLikelihood, CoxPartialLifetimeLikelihood
-from relife.lifetime_model._base import DefaultLifetimeLikelihood
-
-__all__ = ["IntervalLifetimeLikelihood", "CoxPartialLifetimeLikelihood"]
--- relife/likelihood/_base.py
+++
@@ -1,5 +0,0 @@
-# TODO : déplacer dans relife.base (avoir circular import)
-
-
-
-# TODO : mettre dans relife.base, à côté de ParametricModel (evite les imports circulaire)
