BUGFIX : fix ic estimation in SemiParamPH and dtype bug in hessian estimation of Exponential

relife/lifetime_model/_distribution.py
@@ -23,13 +23,19 @@ from scipy.special import digamma, exp1, gamma, gammaincc, gammainccinv
 from typing_extensions import override
 
 from relife.base import FittingResults
-from relife.typing import AnyFloat, NumpyBool, NumpyFloat, MaximumLikelihoodOptimizerOptions, Seed
+from relife.typing import (
+    AnyFloat,
+    MaximumLikelihoodOptimizerOptions,
+    NumpyBool,
+    NumpyFloat,
+    Seed,
+)
 from relife.utils.quadrature import laguerre_quadrature, legendre_quadrature
 
 from ._base import (
-    LifetimeLikelihood,
     FittableParametricLifetimeModel,
     LifetimeData,
+    LifetimeLikelihood,
     ParametricLifetimeModel,
     document_args,
 )
@@ -213,7 +219,9 @@ class LifetimeDistribution(FittableParametricLifetimeModel[()], ABC):
         optimizer = LifetimeLikelihood(self, time, model_args, event, entry)
 
         if "x0" not in optimizer_options:
-            optimizer_options["x0"] = init_distrib_params_from_lifetimes(self, optimizer.data)
+            optimizer_options["x0"] = init_distrib_params_from_lifetimes(
+                self, optimizer.data
+            )
         if "bounds" not in optimizer_options:
             optimizer_options["bounds"] = get_distrib_params_bounds(self)
         if "approx_hessian_method" not in optimizer_options:
@@ -230,7 +238,7 @@ class LifetimeDistribution(FittableParametricLifetimeModel[()], ABC):
 
 
 def init_distrib_params_from_lifetimes(
-        model: LifetimeDistribution, data: LifetimeData
+    model: LifetimeDistribution, data: LifetimeData
 ) -> NDArray[np.float64]:
     # flatten censored_time in case it is 2D
     all_time_values = np.concatenate(
@@ -295,7 +303,7 @@ class Exponential(LifetimeDistribution):
         super().__init__(rate=rate)
 
     @property
-    def rate(self):  # optional but better for clarity and type checking
+    def rate(self) -> float:  # optional but better for clarity and type checking
         """
         Returns the rate value.
 
@@ -313,7 +321,7 @@ class Exponential(LifetimeDistribution):
     @override
     @document_args(base_cls=FittableParametricLifetimeModel, args_docstring=[])
     def chf(self, time: AnyFloat) -> NumpyFloat:
-        return np.asarray(self.rate, dtype=np.float64) * time
+        return np.asarray(self.rate) * time
 
     @override
     @document_args(
@@ -1127,9 +1135,7 @@ class MinimumDistribution(FittableParametricLifetimeModel[*tuple[AnyInt, *Ts]]):
         return self.baseline._get_params_bounds()
 
     @override
-    def _init_params_from_lifetimes(
-            self, data: LifetimeData
-    ) -> NDArray[np.float64]:
+    def _init_params_from_lifetimes(self, data: LifetimeData) -> NDArray[np.float64]:
         return self.baseline._init_params_from_lifetimes(data)
 
     @override

relife/lifetime_model/_semi_parametric.py
@@ -130,18 +130,13 @@ class _BreslowBaseline:
     data: CoxData
     covar_effect: LinearCovarEffect
 
-    def __init__(self,
-                 covar_effect: LinearCovarEffect,
-                 data: CoxData
-                 ):
+    def __init__(self, covar_effect: LinearCovarEffect, data: CoxData):
         assert data.covar.shape[-1] == covar_effect.nb_params
         self.covar_effect = covar_effect
         self.data = data
 
     @overload
-    def chf(
-        self, se: Literal[False], kp: bool = False
-    ) -> NDArray[np.float64]: ...
+    def chf(self, se: Literal[False], kp: bool = False) -> NDArray[np.float64]: ...
     @overload
     def chf(
         self, se: Literal[True], kp: bool = False
@@ -303,31 +298,32 @@ class SemiParametricProportionalHazard:
             return (
                 self._sf["timeline"],
                 self._sf["baseline_estimation"] ** self.covar_effect.g(covar),
-                self._sf["baseline_estimation"] ** self.covar_effect.g(covar) * np.sqrt(self._q1_q2_sum(covar))
+                self._sf["baseline_estimation"] ** self.covar_effect.g(covar)
+                * np.sqrt(self._q1_q2_sum(covar)),
             )
         return (
             self._sf["timeline"],
-            self._sf["baseline_estimation"] ** self.covar_effect.g(covar)
+            self._sf["baseline_estimation"] ** self.covar_effect.g(covar),
         )
 
-    def _q1_q2_sum(
-            self,
-            covar: NDArray[np.float64]
-    ) -> NDArray[np.float64] | None:
+    def _q1_q2_sum(self, covar: NDArray[np.float64]) -> NDArray[np.float64] | None:
         """Voir Klein and Moeschberger: Survival Analysis Techniques for Censored and Truncated Data (p 284)"""
         # TODO: Is it somehow dependent to Breslow estimator ? I don't think so, but the reference isn't clear enough.
         # TODO: Faut-il en faire une fonction et non une méthode ?
         # TODO: estimation foireuse...
         if self.fitting_results is None:
-            raise ValueError("This method can only be called in other methods after model fit")
+            raise ValueError(
+                "This method can only be called in other methods after model fit"
+            )
         if self.fitting_results.covariance_matrix is not None:
             psi_values = psi(self.covar_effect, self._training_data)
             psi_order_1 = psi(self.covar_effect, self._training_data, order=1)
             d_j_on_psi = self._training_data.event_count[:, None] / psi_values
 
             q3 = np.cumsum(
-                (psi_order_1 / psi_values)[None, :, :] - covar[:, None, :]
-                * d_j_on_psi[None, :, :], axis=1
+                ((psi_order_1 / psi_values)[None, :, :] - covar[:, None, :])
+                * d_j_on_psi[None, :, :],
+                axis=1,
             )  # [m: new sample for inference, t: timeline, p]
             q2 = np.squeeze(
                 np.matmul(
@@ -355,13 +351,19 @@ class SemiParametricProportionalHazard:
             (None,) * np.atleast_2d(np.asarray(covar, dtype=np.float64)).shape[-1]
         )
 
-        _,  event_count = np.unique(time[event == 1], return_counts=True)
+        _, event_count = np.unique(time[event == 1], return_counts=True)
         if (event_count > 3).any():  # efron
-            likelihood = EfronPartialLifetimeLikelihood(self.covar_effect, time, covar, event, entry)
+            likelihood = EfronPartialLifetimeLikelihood(
+                self.covar_effect, time, covar, event, entry
+            )
         elif (event_count <= 3).all() and (2 in event_count):  # breslow
-            likelihood = BreslowPartialLifetimeLikelihood(self.covar_effect, time, covar, event, entry)
+            likelihood = BreslowPartialLifetimeLikelihood(
+                self.covar_effect, time, covar, event, entry
+            )
         else:
-            likelihood = CoxPartialLifetimeLikelihood(self.covar_effect, time, covar, event, entry)
+            likelihood = CoxPartialLifetimeLikelihood(
+                self.covar_effect, time, covar, event, entry
+            )
 
         if "x0" not in optimizer_options:
             np.random.seed(1)
@@ -371,7 +373,9 @@ class SemiParametricProportionalHazard:
         optimizer_options["jac"] = likelihood.jac_negative_log
         optimizer_options["hess"] = likelihood.hess_negative_log
 
-        self.fitting_results = likelihood.maximum_likelihood_estimation(**optimizer_options)
+        self.fitting_results = likelihood.maximum_likelihood_estimation(
+            **optimizer_options
+        )
         self.covar_effect.params = self.fitting_results.optimal_params
         self._training_data = likelihood.data
 
@@ -691,3 +695,4 @@ class EfronPartialLifetimeLikelihood(
         hessian_part_2 = hessian_part_2.sum(axis=1)
 
         return hessian_part_1.sum(axis=0) - hessian_part_2.sum(axis=0)
+